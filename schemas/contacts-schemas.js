4223a38d2c78ab91f26b104a36a32163f1667956
KseniaLF <llf9391@gmail.com>
2023-06-18 14:46:02 +0300

    connect with my front

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -2,10 +2,8 @@ const Joi = require("joi");
 
 const contactAddSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string(),
   phone: Joi.string().required(),
-}).messages({
-  "any.required": "missing fields",
 });
 
 module.exports = {
